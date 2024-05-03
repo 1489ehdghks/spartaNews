adb9b9a9c8100254d509b510a0081be2661415b6
tia <duqrl3000@gmail.com>
2024-05-03 15:48:13 +0900

    feat : 댓글조회_1

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .serializers import ArticleSerializer, CommentSerializer
 
-# Create your views here.
+
+# 댓글 조회하기
+class CommentListAPIView(APIView):
+    def get(self, request, article_id):
+        article = get_object_or_404(Article, pk=article_id)
+        comments = article.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, article_id):
+        pass
