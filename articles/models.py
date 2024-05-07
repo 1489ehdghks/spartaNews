c2392de0e1b2d8a7c1541f1d1bc7f0ecac393b90
tia <duqrl3000@gmail.com>
2024-05-07 15:51:26 +0900

    Update : 댓글 표시변경

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.conf import settings
 
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    # 작성자 필드 추가
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Article(models.Model) :
     title = models.CharField(max_length=50)
     content = models.TextField()
@@ -16,11 +25,3 @@ class Article(models.Model) :
         related_name = 'like_articles',
         null = True
         )
-
-class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField()
-    # 작성자 필드 추가
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
--- articles/serializers.py
+++ articles/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 from .models import Article, Comment
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ("article",)
+
+    # 댓글에 article 표시 없앰
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret.pop("article")
+        return ret
+
 class ArticleDetailSerializer(serializers.ModelSerializer):
-    like_count = serializers.IntegerField(read_only = True)
     comments = CommentSerializer(many = True, read_only = True)
+    # 댓글 수 표시 
+    comments_count = serializers.IntegerField(source="comments.count", read_only=True)
+    like_count = serializers.IntegerField(read_only = True)
 
     class Meta :
         model = Article
@@ -14,8 +28,3 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
         representation['like_count'] = instance.like_count.count()
         return representation
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = "__all__"
-        read_only_fields = ("article",)
